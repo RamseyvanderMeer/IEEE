9d5ad8c24dbda1a237496f026806ec17d9230ec9
Ramsey <ramseyv2@illinois.edu>
2021-12-25 22:17:03 -1000

    Styling of fullCalendar

--- client/src/components/homepages/subpages/Calendar.js
+++ client/src/components/homepages/subpages/Calendar.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import './Calendar.scss'
+import React, { Component } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import scrollGridPlugin from '@fullcalendar/scrollgrid'
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import interactionPlugin from "@fullcalendar/interaction";
+import './Calendar.scss'
 
 require('dotenv').config()
 
 const API = process.env.REACT_APP_GOOGLE_API_KEY
 
-console.log(API)
-
-const Calendar = () => {
+export default class Calendar extends Component {
 
 
-    let INITIAL_EVENTS = [
-        {
-            googleCalendarId: '4na3juef6r10antnbtmniav7nc@group.calendar.google.com',
-            className:'event'
-        }
-    ]
-
-    return (
-        <div className="calendar">
-            <div className="calendar__container">
-                <FullCalendar
-                    plugins={[dayGridPlugin, googleCalendarPlugin]}
-                    initialView="dayGridMonth"
-                    googleCalendarApiKey={API}
-                    eventSources={INITIAL_EVENTS}
-                />
+    render() {
+        let INITIAL_EVENTS = [
+            {
+                googleCalendarId: '4na3juef6r10antnbtmniav7nc@group.calendar.google.com',
+                className: 'event'
+            }
+        ]
+        return (
+            <div className="calendar">
+                <div className="calendar__container">
+                    <FullCalendar
+                        plugins={[dayGridPlugin, googleCalendarPlugin, timeGridPlugin, scrollGridPlugin, interactionPlugin]}
+                        initialView="timeGridWeek"
+                        googleCalendarApiKey={API}
+                        eventSources={INITIAL_EVENTS}
+                        allDaySlot={false}
+                        height='80vh'
+                        expandRows={true}
+                        headerToolbar={{
+                            left: "today timeGridDay,timeGridWeek,dayGridMonth",
+                            center: "title",
+                            right: "prev,next"
+                        }}
+                        slotMinTime="10:00:00"
+                        slotMaxTime="20:00:00"
+                        dayMinWidth={200}
+                        stickyFooterScrollbar={true}
+                    />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default Calendar
